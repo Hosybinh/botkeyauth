25e7990caddc6c6c53b890d3709156ef6e998a2b
Hosybinh <138645804+Hosybinh@users.noreply.github.com>
2025-09-30 20:54:29 +0700

    Update botkeyauth.py

--- botkeyauth.py
+++ botkeyauth.py
@@ -1,5 +1,5 @@
-import discord
-from discord.ext import commands
+import nextcord as discord
+from nextcord.ext import commands
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import random
